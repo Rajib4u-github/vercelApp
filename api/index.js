52ff8803b30af2bb9be8b39ffda1ed88d4866efc
Rajib4u-github <rajibkarmakar4u@gmail.com>
2025-05-26 01:38:05 +0530

    Save latest changes.

--- api/index.js
+++ api/index.js
@@ -16,6 +16,43 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../frontend/components', 'home.html'));
 });
 
+app.get('/company',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'company.html'));
+});
+
+app.get('/companyv2',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'companyv2.html'));
+});
+
+app.get('/website',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'website.html'));
+});
+
+app.get('/admin',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'admin.html'));
+});
+
+app.get('/stock',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'stock.html'));
+});
+
+app.get('/profile',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'profile.html'));
+});
+
+
+app.get('/coching',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'coching.html'));
+});
+
+app.get('/ecomerce',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'ecomerce.html'));
+});
+
+app.get('/portfolio',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/components', 'portfolio.html'));
+});
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
