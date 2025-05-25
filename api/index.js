5fd9338277770d9846ed949ba7a36b829e8f3449
Rajib4u-github <rajibkarmakar4u@gmail.com>
2025-05-25 19:52:21 +0530

    savce latest changes.

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors');
 // app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.use(cors()); 
+
+// app.get('/',function(req,res){
+//     res.sendFile('./frontend/components/home.html', { root: __dirname });
+// });
+
 app.get('/',function(req,res){
-    res.sendFile('./frontend/components/home.html', { root: __dirname });
+    res.sendFile('./frontend/components/home.html');
 });
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
