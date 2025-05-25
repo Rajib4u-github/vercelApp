59eaf16bb94732a55e9058176f7ad5316e66d702
Rajib4u-github <rajibkarmakar4u@gmail.com>
2025-05-25 19:04:28 +0530

    savce latest changes.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 
 // app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use('/', function(req, res) {
-    res.sendFile('./frontend/components/home.html');
+app.get('/',function(req,res){
+    res.sendFile('./frontend/components/home.html', { root: __dirname });
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
