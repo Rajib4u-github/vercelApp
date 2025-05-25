c3c7dea5bf6863cf9d4318406d9b6d36bd6b8c8d
Rajib4u-github <rajibkarmakar4u@gmail.com>
2025-05-25 23:23:55 +0530

    Save latest changes.

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-// app.get("/", (req, res) => res.send("Express on Vercel"));
+const path = require('path');
 
+// app.get("/", (req, res) => res.send("Express on Vercel"));
+app.set("view engine", "html");
+app.set('views', path.join(__dirname, 'frontent/components'));
+app.engine('html', require('ejs').renderFile);
 app.use(cors()); 
 
-// app.get('/',function(req,res){
-//     res.sendFile('./frontend/components/home.html', { root: __dirname });
-// });
+
+//console.log(path.join(__dirname, '../frontend/components', 'home.html'));
 
 app.get('/',function(req,res){
-    res.sendFile('./frontend/components/home.html');
+    res.sendFile(path.join(__dirname, '../frontend/components', 'home.html'));
 });
 
 
