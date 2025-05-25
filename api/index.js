02cb319c6590db6162364fa477fc7d530df37086
Rajib4u-github <rajibkarmakar4u@gmail.com>
2025-05-25 18:31:04 +0530

    savce latest changes.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use('/', function(req, res) {
+    res.sendfile('./client/camera.html');
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
